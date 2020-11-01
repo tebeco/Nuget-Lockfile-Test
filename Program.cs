6fc88766648eb5dc8a600cc41df6d74da5cec412
GGG KILLER <gggkiller2@gmail.com>
2020-11-01 01:31:34 -0300

    Add base files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Nuget_Lockfile_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
